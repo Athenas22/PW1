7ec9d5900b9f5231b19b3213baffea0c5c782b66
Athena22 <mlauragdf22@gmail.com>
2026-03-24 23:12:45 -0300

    Fazendo ajustes finais

--- dist/Weight.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Gabarito {
-    numero: number;
-    peso: number;
-    respostaCorreta: string;
-}
-export declare const gabaritoProva: Gabarito[];
-//# sourceMappingURL=Weight.d.ts.map
